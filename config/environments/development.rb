cd4a2ec8bf8083c03c8ada11a131be88553cf029
polttergeist <ivan010201@gmail.com>
2022-10-15 02:07:09 +0300

    configuring heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   config.hosts << 'localhost:3000'
